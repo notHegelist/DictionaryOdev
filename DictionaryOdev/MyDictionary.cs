57da4658f23302aeab26d9a7b3319925b3643d0e
notHegelist <furkangosker@gmail.com>
2021-02-22 04:23:17 +0300

    Add project files.

---
+++ DictionaryOdev/MyDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryOdev
+{
+
+    public class MyDictionary<TKey, TValue>
+    {
+        TKey[] _arrayK;
+        TKey[] _tempArrayK;
+        TValue[] _arrayV;
+        TValue[] _tempArrayV;
+
+
+        public MyDictionary()
+        {
+            _arrayK = new TKey[0];
+            _arrayV = new TValue[0];
+        }
+        public void Add(TKey plaque, TValue city)
+        {
+            _tempArrayK = _arrayK;
+            _tempArrayV = _arrayV;
+            _arrayK = new TKey[_arrayK.Length + 1];
+            _arrayV = new TValue[_arrayV.Length + 1];
+
+            for (int i = 0; i < _tempArrayK.Length; i++)
+            {
+                _arrayK[i] = _tempArrayK[i];
+                _arrayV[i] = _tempArrayV[i];
+            }
+            _arrayK[_arrayK.Length - 1] = plaque;
+            _arrayV[_arrayV.Length - 1] = city;
+        }
+        public int Count
+        {
+            get { return _arrayK.Length; }
+        }
+        public TValue this[TKey plaque]
+        {
+            get
+            {
+                return _arrayV[Array.IndexOf(_arrayK, plaque)];
+            }
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ DictionaryOdev/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DictionaryOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cities = new MyDictionary<int, string>();
+
+            cities.Add(10, "Balıkesir");
+            cities.Add(11, "Bilecik");
+            cities.Add(16, "Bursa");
+            cities.Add(17, "Çanakkale");
+            cities.Add(22, "Edirne");
+            cities.Add(34, "İstanbul");
+            cities.Add(39, "Kırklareli");
+            cities.Add(41, "Kocaeli");
+            cities.Add(54, "Sakarya");
+            cities.Add(59, "Tekirdağ");
+            cities.Add(77, "Yalova");
+
+            Console.WriteLine("Marmara Bölgesi İllerinden Birinin Plaka Numarasını Giriniz.");
+            int plaque = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(cities[plaque]);
+            }
+            catch 
+            {
+
+                Console.WriteLine("Hatalı Numara Girdiniz.");
+                Console.WriteLine("");
+                Console.WriteLine("Girdiğiniz Değer Marmara Bölgesine Ait Bir Plaka Numarası Değildir.");
+
+            }
+        }
+    }
+}
